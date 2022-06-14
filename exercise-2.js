c2810e1bcf3fcfd1cac405de065241d43d66b431
alantheandroid <alessio.ilardi@gmail.com>
2022-06-14 20:17:42 +0200

    feat: add one-line destructuring assignment

--- exercise-2.js
+++ exercise-2.js
@@ -1,13 +1,10 @@
 const person = {
   id: 1,
-  firstName: 'Mario',
-  lastName: 'Rossi',
-  age: 25
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
 };
 
-const id = person.id;
-const firstName = person.firstName;
-const lastName = person.lastName;
-const age = person.age;
+const { id, firstName, lastName, age } = person;
 
 console.log(id, firstName, lastName, age);
